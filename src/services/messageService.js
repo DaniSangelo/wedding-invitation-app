bcf61a804b6c9c7a37fcbcb490cc703a0fb82d1e
Daniel Sângelo <danielsangelo@hotmail.com>
2025-05-19 21:48:06 -0300

    Alter message [enhancement]
    - Add function to get diff days between current date and target date
    - Add function to remove non-digit characters
    - Modifying .gitignore
    - Install dotenv

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,5 +1,6 @@
 const { MessageMedia } = require('whatsapp-web.js');
-const { delay, getImagePath } = require('../utils/helpers');
+const { delay, getImagePath, diffDays, formatPhone } = require('../utils/helpers');
+require('dotenv').config();
 
 class MessageService {
     constructor(client) {
@@ -7,30 +8,39 @@ class MessageService {
     }
 
     async sendInvitation(guest) {
-        const chatId = `${guest.phone}@c.us`;
-        const caption = `🌸 Teste bot - Mark VI - Olá, ${guest.name}! Aqui está o convite do nosso casamento. Sua mesa será a número ${guest.table}. 💍`;
+        const phone = formatPhone(guest.phone);
+        const days = diffDays();
+        const chatId = `${phone}@c.us`;
+        const mainMessage = `S A V E · T H E · D A T E \n
+🗓️ ${process.env.CALENDAR_URL} \n
+Faltam ${days} dias para o nosso tão sonhado casamento!! 💍\n
+Estamos muito felizes por você fazer parte desse momento tão especial. \n
+E para não deixar você perder nada, já deixa salvo aí na sua agenda. 😉\n
+Nos vemos em breve! 🥂🎉 \n
+Carinhosamente, \n
+Gih & Dani`;
         
         try {
             const media = await MessageMedia.fromFilePath(getImagePath());
-            console.log(`📨 Enviando imagem para ${guest.name} (${chatId})...`);
-            await this.client.sendMessage(chatId, media, { caption });
-            console.log(`✔️ Convite enviado para ${guest.name}`);
+            console.log(`📨 Sending image to ${guest.name} (${chatId})...`);
+            await this.client.sendMessage(chatId, media, { caption: mainMessage });
+            console.log(`✔️ Invite sent to ${guest.name}`);
             return true;
         } catch (err) {
-            console.error(`❌ Erro ao enviar para ${guest.name}:`, err.message);
+            console.error(`❌ Error sending to ${guest.name} (${phone}):`, err.message);
             return false;
         }
     }
 
     async sendInvitationsToAllGuests(guests) {
-        console.log('✅ Conectado. Enviando convite...');
+        console.log('✅ Connecting. Sending invitation...');
         
         for (const guest of guests) {
             await this.sendInvitation(guest);
-            await delay(2000); // pausa para evitar bloqueios
+            await delay(3000);
         }
 
-        console.log('📤 Todos os convites foram enviados!');
+        console.log('📤 All invitations have been sent!');
     }
 }
 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,24 @@
 const path = require('path');
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-const getImagePath = () => path.join(__dirname, '../resources/save-the-date.png');
+const getImagePath = () => path.join(__dirname, '../resources/save-the-date-v2.png');
+
+const diffDays = () => {
+    const targetDate = new Date(2025, 4, 31); // Months are 0-indexed: 4 = May
+    const today = new Date();
+    targetDate.setHours(0,0,0,0);
+    today.setHours(0,0,0,0);
+    const diffTime = targetDate - today;
+    const days = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return days;
+};
+
+const formatPhone = (phone) => {
+    return phone.replace(/\D/g, '');
+};
 
 module.exports = {
     delay,
-    getImagePath
+    getImagePath,
+    diffDays,
+    formatPhone
 }; 
